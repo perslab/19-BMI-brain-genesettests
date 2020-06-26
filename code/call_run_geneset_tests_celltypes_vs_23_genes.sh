72f789a5aafbe0639278f19ad8e1cffb78607252
JonThom <jonatan.thompson@gmail.com>
2020-06-26 15:07:32 +0200

    update script to new gtex id

--- code/call_run_geneset_tests_celltypes_vs_23_genes.sh
+++ code/call_run_geneset_tests_celltypes_vs_23_genes.sh
@@ -2,7 +2,7 @@
 # usage: bash call_run_geneset_tests_celltypes_vs_BMIs.sh &> log_genesettests_celltypes_vs_BMI_191007.txt
 
 
-ES_DIR=/projects/timshel/sc-genetics/timshel-bmicelltypes2019/out/es/
+ES_DIR=/projects/jonatan/pub-perslab/timshel-bmicelltypes2019/out/es
 PREFIX_RUN=BMI_23_genes
 DIR_OUT=../output/
 PATH_GENESETS=../data/list_23_prot_mono_early_extr_obesitygenes_ENSG.RDS
@@ -14,7 +14,7 @@ DOPAR=FALSE
 NCORES=0
 NREP=0
   
-declare -a DATASETS=("campbell2017_lvl1" "campbell2017_lvl2" "chen2017" "romanov2017" "mousebrain" "moffitt2018" "mikkelsen2019" "kimVMH2019_smartseq" "kimVMH2019_10x" "tabula_muris")
+declare -a DATASETS=("campbell2017_lvl1" "campbell2017_lvl2" "chen2017" "romanov2017" "mousebrain" "moffitt2018" "mikkelsen2019" "kimVMH2019_smartseq" "kimVMH2019_10x" "tabula_muris" "gtex_v8_filtered_normLog_all")
   
 for (( i=0; i<${#DATASETS[@]}; i++ )); do
     DATASET=${DATASETS[$i]}
